5b8cfd859af301f82d0e11bb3c264ac4d9c2f0ea
Liam Pattinson <liampattinson@gmail.com>
2020-12-26 11:25:31 +0000

    Updated conky, fixed window maximising issues
    Tint2 was preventing window maximisation on dual screens.
    If any problems develop on other systems, try tinkering with
    the panel_monitor setting. Set it to 'all' to be safe,
    or try setting it to 0 or 1.

--- conky/.config/conky/hex.lua
+++ conky/.config/conky/hex.lua
@@ -22,22 +22,24 @@ font_face=CAIRO_FONT_WEIGHT_NORMAL
 
 -- Colours
 
--- Colour white
-cw_r,cw_g,cw_b=1,1,1
--- Colour grey
-cg_r,cg_g,cg_b=0.7,0.7,0.7
--- Colour blue
-cb_r,cb_g,cb_b=38/255,139/255,210/255
--- Colour black
-ck_r,ck_g,ck_b=0,0,0
+-- white
+cw = {r=1,g=1,b=1}
+-- grey
+cg = {r=0.7,g=0.7,b=0.7}
+-- blue
+cb = {r=38/255,g=139/255,b=210/255}
+-- black
+ck = {r=0,g=0,b=0}
 
--- Hexagon Utilities
+-- Utilities
 
--- Draw hexagon, given left-most point
-function draw_hex_helper( cr, x, y, length, width, r, g, b, a)
-    cairo_set_source_rgba(cr,r,g,b,a)
-    cairo_set_line_width(cr,width)
-    cairo_move_to(cr,x,y)
+-- Draw hexagon, given centre point
+
+function draw_hex_helper( cr, x, y, length, linewidth, color, opaqueness)
+    cairo_set_source_rgba(cr,color.r,color.g,color.b,opaqueness)
+    cairo_set_line_width(cr,linewidth)
+    xstart = x-length*(0.5+cos60)
+    cairo_move_to(cr,xstart,y)
     cairo_rel_line_to(cr, length*cos60, length*sin60)
     cairo_rel_line_to(cr, length, 0)
     cairo_rel_line_to(cr, length*cos60, -length*sin60)
@@ -47,51 +49,55 @@ function draw_hex_helper( cr, x, y, length, width, r, g, b, a)
     cairo_close_path(cr)
     cairo_stroke(cr)
 end
+
 function draw_hex( cr, x, y, length, width)
-    draw_hex_helper(cr,x,y,length,width,cb_r,cb_g,cb_b,1.0)
+    draw_hex_helper(cr,x,y,length,width,cb,1.0)
     -- Add cheap blur
-    draw_hex_helper(cr,x,y,length,width+10,cb_r,cb_g,cb_b,0.02)
-    draw_hex_helper(cr,x,y,length,width+8,cb_r,cb_g,cb_b,0.05)
-    draw_hex_helper(cr,x,y,length,width+6,cb_r,cb_g,cb_b,0.07)
-    draw_hex_helper(cr,x,y,length,width+4,cb_r,cb_g,cb_b,0.1)
-    draw_hex_helper(cr,x,y,length,width+3,cb_r,cb_g,cb_b,0.2)
-    draw_hex_helper(cr,x,y,length,width+2,cb_r,cb_g,cb_b,0.3)
+    draw_hex_helper(cr,x,y,length,width+10,cb,0.02)
+    draw_hex_helper(cr,x,y,length,width+8,cb,0.05)
+    draw_hex_helper(cr,x,y,length,width+6,cb,0.07)
+    draw_hex_helper(cr,x,y,length,width+4,cb,0.1)
+    draw_hex_helper(cr,x,y,length,width+3,cb,0.2)
+    draw_hex_helper(cr,x,y,length,width+2,cb,0.3)
     -- This solution will, unfortunately, result in thicker
     -- lines in between hexagones than at the outer edges.
     -- The effect is strongest where 3 hexes meet. Much simpler
     -- than doing a real blur with cairo though...
 end
 
--- Given left-most point on hexagon, get top right point
-function to_hex_tr( x, y, length)
-    return x+length*(1+cos60), y-length*sin60
+-- Move around hexagon grid
+
+function hex_move(x,y,length,dir)
+    xleft = x-length*(0.5+cos60)
+    xright = x+length*(0.5+cos60)
+    if dir=="top left" then
+        return xleft - length*cos60, y - length*sin60
+    elseif dir=="bottom left" then
+        return xleft - length*cos60, y + length*sin60
+    elseif dir=="top right" then
+        return xright + length*cos60, y - length*sin60
+    elseif dir=="bottom right" then
+        return xright + length*cos60, y + length*sin60
+    elseif dir=="top" then
+        return x, y - 2*length*sin60
+    elseif dir=="bottom" then
+        return x, y + 2*length*sin60
+    else
+        return x,y
+    end
 end
 
--- Given top-right point on hexagon, get left point
-function from_hex_tr( x, y, length)
-    return x-length*(1+cos60), y+length*sin60
-end
+-- Draw line, given start and end points
 
--- Given left-most point on hexagon, get bottom right point
-function to_hex_br( x, y, length)
-    return x+length*(1+cos60), y+length*sin60
+function draw_line( cr, xstart, xend, ystart, yend, width, color, opaqueness)
+    cairo_set_source_rgba(cr,color.r,color.g,color.b,opaqueness)
+    cairo_set_line_width(cr,width)
+    cairo_move_to(cr,xstart,ystart)
+    cairo_rel_line_to(cr, xend-xstart, yend-ystart)
+    cairo_close_path(cr)
+    cairo_stroke(cr)
 end
-
--- Given bottom-right point on hexagon, get left point
-function from_hex_br( x, y, length)
-    return x-length*(1+cos60), y-length*sin60
-end
-
--- Given left point of hexagon, get center
-function to_hex_c( x, y, length)
-    return x+length*(0.5+cos60), y
-end
-
--- Given center of hexagon, get left point
-function from_hex_c( x, y, length)
-    return x-length*(0.5+cos60), y
-end
-
+        
 -- Text Utils
 
 -- Write center-aligned text
@@ -105,59 +111,66 @@ end
 
 -- Gauges
 
-function draw_gauge( cr, x, y, fill, radius)
+function draw_circle_gauge( cr, x, y, fill, radius)
     ring_width=8
     ring_start=rad(90)
     ring_end=rad(360)
     -- Background
     cairo_set_line_width(cr,ring_width)
-    cairo_set_source_rgba(cr,cw_r,cw_g,cw_b,0.6)
-    cairo_arc(cr,x,y,radius,ring_start-0.02,ring_end+0.02)
+    cairo_set_source_rgba(cr,cw.r,cw.g,cw.b,0.4)
+    cairo_arc(cr,x,y,radius,ring_start-0.04,ring_end+0.04)
     cairo_stroke(cr)
     -- Cheap blur
     cairo_set_line_width(cr,ring_width+2)
-    cairo_set_source_rgba(cr,cw_r,cw_g,cw_b,0.2)
-    cairo_arc(cr,x,y,radius,ring_start-0.04,ring_end+0.04)
+    cairo_set_source_rgba(cr,cw.r,cw.g,cw.b,0.2)
+    cairo_arc(cr,x,y,radius,ring_start-0.08,ring_end+0.08)
     cairo_stroke(cr)
-    cairo_set_line_width(cr,ring_width+4)
-    cairo_set_source_rgba(cr,cw_r,cw_g,cw_b,0.1)
-    cairo_arc(cr,x,y,radius,ring_start-0.04,ring_end+0.04)
+    cairo_set_line_width(cr,ring_width+6)
+    cairo_set_source_rgba(cr,cw.r,cw.g,cw.b,0.1)
+    cairo_arc(cr,x,y,radius,ring_start-0.12,ring_end+0.12)
     cairo_stroke(cr)
     -- Black fill
     cairo_set_line_width(cr,ring_width-4)
-    cairo_set_source_rgb(cr,ck_r,ck_g,ck_b)
+    cairo_set_source_rgb(cr,ck.r,ck.g,ck.b)
     cairo_arc(cr,x,y,radius,ring_start,ring_end)
     cairo_stroke(cr)
     -- Blue meter
     meter_end=ring_start + (ring_end-ring_start)*fill
     cairo_set_line_width(cr,ring_width-6)
-    cairo_set_source_rgb(cr,cb_r,cb_g,cb_b)
+    cairo_set_source_rgb(cr,cb.r,cb.g,cb.b)
     cairo_arc(cr,x,y,radius,ring_start,meter_end)
     cairo_stroke(cr)
-    -- Cheap blur
-    cairo_set_line_width(cr,ring_width-4)
-    cairo_set_source_rgba(cr,cb_r,cb_g,cb_b,0.8)
-    cairo_arc(cr,x,y,radius,ring_start,meter_end)
-    cairo_stroke(cr)
-    cairo_set_line_width(cr,ring_width)
-    cairo_set_source_rgba(cr,cb_r,cb_g,cb_b,0.5)
-    cairo_arc(cr,x,y,radius,ring_start,meter_end)
-    cairo_stroke(cr)
+end
+
+function draw_flat_gauge( cr, x, y, fill, width)
+    height=8
+    line_start=x-0.5*width
+    line_end=x+0.5*width
+    -- Background
+    draw_line(cr,line_start-2,line_end+2,y,y,height,cw,0.4)
+    -- Blur
+    draw_line(cr,line_start-4,line_end+4,y,y,height+2,cw,0.2)
+    draw_line(cr,line_start-8,line_end+8,y,y,height+4,cw,0.1)
+    -- Black fill
+    draw_line(cr,line_start,line_end,y,y,height-4,ck,1.0)
+    -- Blue meter
+    meter_end = line_start + fill*(line_end-line_start)
+    draw_line(cr,line_start,meter_end,y,y,height-6,cb,1.0)
 end
 
 -- Colours
 
 function set_white(cr)
-    cairo_set_source_rgb(cr,cw_r,cw_g,cw_b)
+    cairo_set_source_rgb(cr,cw.r,cw.g,cw.b)
 end
 function set_grey(cr)
-    cairo_set_source_rgb(cr,cg_r,cg_g,cg_b)
+    cairo_set_source_rgb(cr,cg.r,cg.g,cg.b)
 end
 function set_blue(cr)
-    cairo_set_source_rgb(cr,cb_r,cb_g,cb_b)
+    cairo_set_source_rgb(cr,cb.r,cb.g,cb.b)
 end
 function set_black(cr)
-    cairo_set_source_rgb(cr,ck_r,ck_g,ck_b)
+    cairo_set_source_rgb(cr,ck.r,ck.g,ck.b)
 end
 
 -- Main function
@@ -190,95 +203,83 @@ function conky_main()
     hex_width=1
     gauge_radius=50
 
-    -- Begin drawing
-    x,y=10,500
+    -- Begin drawing in bottom-right corner
+    x,y=500,500
+    -- Save starting point. We'll begin going left, then going up
     x_start,y_start=x,y
 
     -- Decorative hex
     draw_hex(cr,x,y,hex_length,hex_width)
-    x,y=to_hex_tr(x,y,hex_length)
 
     -- CPU hex
+    x,y=hex_move(x,y,hex_length,"top left")
     draw_hex(cr,x,y,hex_length,hex_width)
     cpu=tonumber(conky_parse('${cpu}'))
     cpu_max=100
-    x,y=to_hex_c(x,y,hex_length)
     set_white(cr)
     write_text_centered(cr,x,y-5,"CPU",extents)
     set_grey(cr)
     write_text_centered(cr,x,y+5,tostring(cpu).."%",extents)
-    draw_gauge(cr,x,y,cpu/cpu_max,gauge_radius)
-    x,y=from_hex_c(x,y,hex_length)
+    draw_circle_gauge(cr,x,y,cpu/cpu_max,gauge_radius)
 
     -- RAM hex
-    x,y=to_hex_br(x,y,hex_length)
+    x,y=hex_move(x,y,hex_length,"bottom left")
     draw_hex(cr,x,y,hex_length,hex_width)
     mem=conky_parse('${mem}')
     mem_max=conky_parse('${memmax}')
     mem_perc=tonumber(conky_parse('${memperc}'))
-    x,y=to_hex_c(x,y,hex_length)
     set_white(cr)
     write_text_centered(cr,x,y-10,"RAM",extents)
     set_grey(cr)
     write_text_centered(cr,x,y,mem.."/"..mem_max,extents)
     write_text_centered(cr,x,y+10,tostring(mem_perc).."%",extents)
-    draw_gauge(cr,x,y,mem_perc/100,gauge_radius)
-    x,y=from_hex_c(x,y,hex_length)
+    draw_circle_gauge(cr,x,y,mem_perc/100,gauge_radius)
 
     -- Swap hex
-    x,y=to_hex_tr(x,y,hex_length)
+    x,y=hex_move(x,y,hex_length,"top left")
     draw_hex(cr,x,y,hex_length,hex_width)
     swap=conky_parse('${swap}')
     swap_max=conky_parse('${swapmax}')
     swap_perc=tonumber(conky_parse('${swapperc}'))
-    x,y=to_hex_c(x,y,hex_length)
     set_white(cr)
     write_text_centered(cr,x,y-10,"SWAP",extents)
     set_grey(cr)
     write_text_centered(cr,x,y,swap.."/"..swap_max,extents)
     write_text_centered(cr,x,y+10,tostring(swap_perc).."%",extents)
-    draw_gauge(cr,x,y,swap_perc/100,gauge_radius)
-    x,y=from_hex_c(x,y,hex_length)
+    draw_circle_gauge(cr,x,y,swap_perc/100,gauge_radius)
 
     -- File system hex
-    x,y=to_hex_br(x,y,hex_length)
+    x,y=hex_move(x,y,hex_length,"bottom left")
     draw_hex(cr,x,y,hex_length,hex_width)
     fs=conky_parse('${fs_used}')
     fs_max=conky_parse('${fs_size}')
     fs_perc=tonumber(conky_parse('${fs_used_perc}'))
-    x,y=to_hex_c(x,y,hex_length)
     set_white(cr)
-    write_text_centered(cr,x,y-10,"HDD",extents)
+    write_text_centered(cr,x,y-10,"/",extents)
     set_grey(cr)
     write_text_centered(cr,x,y,fs.."/"..fs_max,extents)
     write_text_centered(cr,x,y+10,tostring(fs_perc).."%",extents)
-    draw_gauge(cr,x,y,fs_perc/100,gauge_radius)
-    x,y=from_hex_c(x,y,hex_length)
+    draw_circle_gauge(cr,x,y,fs_perc/100,gauge_radius)
 
     -- back to start...
     x,y=x_start,y_start
 
     -- Network hex
-    -- (Disabled)
-    x,y=to_hex_tr(x,y,hex_length)
-    x,y=from_hex_br(x,y,hex_length)
-    --draw_hex(cr,x,y,hex_length,hex_width)
-    --x,y=to_hex_c(x,y,hex_length)
-    --set_white(cr)
-    --write_text_centered(cr,x,y-25,"NETWORK",extents)
-    --write_text_centered(cr,x,y-10,"UPSPEED",extents)
-    --set_grey(cr)
-    --write_text_centered(cr,x,y,conky_parse('${upspeed '..wlan..'}'),extents)
-    --set_white(cr)
-    --write_text_centered(cr,x,y+10,"DOWNSPEED",extents)
-    --set_grey(cr)
-    --write_text_centered(cr,x,y+20,conky_parse('${downspeed '..wlan..'}'),extents)
-    --x,y=from_hex_c(x,y,hex_length)
+    x,y=hex_move(x,y,hex_length,"top")
+    draw_hex(cr,x,y,hex_length,hex_width)
+    set_white(cr)
+    write_text_centered(cr,x,y-25,"NETWORK",extents)
+    write_text_centered(cr,x,y-10,"UPSPEED",extents)
+    set_grey(cr)
+    write_text_centered(cr,x,y,conky_parse('${upspeed '..wlan..'}'),extents)
+    set_white(cr)
+    write_text_centered(cr,x,y+10,"DOWNSPEED",extents)
+    set_grey(cr)
+    write_text_centered(cr,x,y+20,conky_parse('${downspeed '..wlan..'}'),extents)
 
     -- Processes hex
-    x,y=to_hex_tr(x,y,hex_length)
+    x,y=hex_move(x,y,hex_length,"top left")
     draw_hex(cr,x,y,hex_length,hex_width)
-    x,y=to_hex_c(x,y,hex_length)
     set_white(cr)
     write_text_centered(cr,x,y-15,"PROCESSES",extents)
     set_grey(cr)
@@ -287,28 +288,24 @@ function conky_main()
     write_text_centered(cr,x,y+5,"RUNNING",extents)
     set_grey(cr)
     write_text_centered(cr,x,y+15,conky_parse('${running_processes}'),extents)
-    x,y=from_hex_c(x,y,hex_length)
 
-    -- Optional hex
-    x,y=from_hex_br(x,y,hex_length)
+    -- Home dir
+    x,y=hex_move(x,y,hex_length,"top right")
     draw_hex(cr,x,y,hex_length,hex_width)
-    if tonumber(conky_parse('${if_mounted /data}1${endif}')) then
-        fs=conky_parse('${fs_used /data}')
-        fs_max=conky_parse('${fs_size /data}')
-        fs_perc=tonumber(conky_parse('${fs_used_perc /data}'))
-        x,y=to_hex_c(x,y,hex_length)
+    if tonumber(conky_parse('${if_mounted /home}1${endif}')) then
+        fs=conky_parse('${fs_used /home}')
+        fs_max=conky_parse('${fs_size /home}')
+        fs_perc=tonumber(conky_parse('${fs_used_perc /home}'))
         set_white(cr)
-        write_text_centered(cr,x,y-10,"/data",extents)
+        write_text_centered(cr,x,y-10,"/home",extents)
         set_grey(cr)
         write_text_centered(cr,x,y,fs.."/"..fs_max,extents)
         write_text_centered(cr,x,y+10,tostring(fs_perc).."%",extents)
-        draw_gauge(cr,x,y,fs_perc/100,gauge_radius)
-        x,y=from_hex_c(x,y,hex_length)
+        draw_circle_gauge(cr,x,y,fs_perc/100,gauge_radius)
     end
 
-    -- Decorative hexes
-    x,y=to_hex_tr(x,y,hex_length)
-    x,y=from_hex_br(x,y,hex_length)
+    -- Decorative hex
+    x,y=hex_move(x,y,hex_length,"top")
     draw_hex(cr,x,y,hex_length,hex_width)
 
     -- clean up cairo
